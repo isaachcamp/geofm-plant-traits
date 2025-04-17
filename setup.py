2ead095dbb6d8da6d68a46317332e90958cd5b04
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-04-17 19:15:15 +0100

    Add setup.py for uv in github actions

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+
+
+setup(name='geofm-plant-traits',
+      description='ML models for predicting plant traits from multispectral satellite data',
+      url='https://github.com/isaachcamp/geofm-plant-traits',
+      author='Isaac Campbell',
+      author_email='isaac.campbell@wolfson.ox.ac.uk',
+      license='GNU GPLv3',
+      packages=find_packages(exclude=["tests", "docs"]),
+      version=0.1.0,
+      zip_safe=False)
