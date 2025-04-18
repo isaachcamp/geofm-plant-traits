571eb11ac061c2305dbe353899e315dcc5f0b3a6
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-04-18 17:59:33 +0100

    Add larger layers and decrease learning rate

--- models/vanilla_nn_bands_only.py
+++ models/vanilla_nn_bands_only.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-from src.data_utils import minmax_scaler
+from src.data_utils import standardise
 
 
 BANDS = [
@@ -27,17 +27,23 @@ class NNBandsOnly(BaseEstimator, RegressorMixin):
         self.seed = seed
         self.model = nn.Sequential(
             nn.Linear(9, 64),
+            nn.GELU(),
             nn.BatchNorm1d(64),
-            nn.ReLU(),
             nn.Linear(64, 128),
+            nn.GELU(),
             nn.BatchNorm1d(128),
-            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.GELU(),
+            nn.BatchNorm1d(256),
+            nn.Linear(256, 128),
+            nn.GELU(),
+            nn.BatchNorm1d(128),
             nn.Linear(128, 64),
+            nn.GELU(),
             nn.BatchNorm1d(64),
-            nn.ReLU(),
             nn.Linear(64, 1)
         )
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0005)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-4)
         self.loss_fn = nn.MSELoss()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
@@ -45,11 +51,14 @@ class NNBandsOnly(BaseEstimator, RegressorMixin):
     def fit(self, X, y):
         # Create DataLoader
         dataset = torch.utils.data.TensorDataset(X, y)
-        dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+        dataloader = DataLoader(dataset, batch_size=100, shuffle=True)
 
         # Train for a fixed number of epochs
         self.model.train()
-        for epoch in range(50):
+        loss_history = []
+
+        for epoch in range(100):
+            running_loss = 0.
             for X_batch, y_batch in dataloader:
                 X_batch = X_batch.to(self.device)
                 y_batch = y_batch.to(self.device)
@@ -61,9 +70,15 @@ class NNBandsOnly(BaseEstimator, RegressorMixin):
                 loss.backward()
                 self.optimizer.step()
 
+                running_loss += loss.item()
+
+            # Average loss over the epoch
+            loss = running_loss / len(dataloader)
+            loss_history.append(loss)
+
             # Print loss every 5 epochs
             if epoch % 5 == 0:
-                print(f"Epoch {epoch}, Loss: {loss.item()}")
+                print(f"Epoch {epoch}, Loss: {loss}")
 
         return self
 
@@ -78,8 +93,8 @@ class NNBandsOnly(BaseEstimator, RegressorMixin):
     def configure_data(self, X, y):
         """Configure the data for the model."""
         # Convert to PyTorch tensors
-        X = minmax_scaler(X[BANDS]).to_numpy()
-        y = y.to_numpy().reshape(-1, 1)
+        X = standardise(X[BANDS].to_numpy(), axis=1)
+        y = standardise(y.to_numpy().reshape(-1, 1))
         X = torch.FloatTensor(X)
         y = torch.FloatTensor(y)
         return X, y
