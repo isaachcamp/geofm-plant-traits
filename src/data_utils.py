63dd9afd54aae74dd3c298148e80e16a20f152ea
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-04-18 10:02:06 +0100

    debug workflow

--- src/data_utils.py
+++ src/data_utils.py
@@ -20,7 +20,7 @@ class LabelledTraitData:
         if not (self.data_path / "train").exists():
             raise FileNotFoundError(f"Train directory {self.data_path / 'train'} does not exist.")
 
-        print(self.data_path / f"train/{self.var}_train_data.csv")
+        print((self.data_path / "train/").glob(f"{self.var}*data*"))
 
         # Load the datasets and labels.
         self.train_data = pd.read_csv(
