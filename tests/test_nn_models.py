845c38b0af5794155ee136bcddc5af1b30da89ab
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-05-16 12:26:46 +0100

    Update NN model tests

--- tests/test_nn_models.py
+++ tests/test_nn_models.py
@@ -17,9 +17,23 @@ X = pd.DataFrame({
 })
 y = pd.Series([1, 2, 3])
 
+# Needed for the fit method.
+X_val = pd.DataFrame({
+    'B2_real': [4],
+    'B3_real': [5],
+    'B4_real': [6],
+    'B5_real': [7],
+    'B6_real': [8],
+    'B7_real': [9],
+    'B8_real': [10],
+    'B11_real': [11],
+    'B12_real': [12],
+})
+y_val = pd.Series([4])
+
 class TestNNBandsOnly:
     def test_init(self):
-        model = NNBandsOnly(seed=42)
+        model = NNBandsOnly(seed=42, var='test_var')
 
         assert model.seed == 42
         assert isinstance(model.model, nn.Sequential)
@@ -28,19 +42,22 @@ class TestNNBandsOnly:
         assert model.device.type in ["cuda", "cpu"]
 
     def test_predict_handles_batch_correctly(self):
-        model = NNBandsOnly(seed=42)
+        model = NNBandsOnly(seed=42, var='test_var')
+        model.set_stats({'mean': 0, 'std': 1})
 
         X_train, y_train = model.configure_data(X, y)
-        model.fit(X_train, y_train)
+        X_val_prep, y_val_prep = model.configure_data(X_val, y_val)
+
+        model.fit(X_train, y_train, X_val_prep, y_val_prep)
 
         preds = model.predict(X_train)
 
         # Check if predictions are made
-        assert preds.shape == torch.Size([3, 1])
+        assert preds.shape == (3, 1)
 
     def test_configure_data(self):
         """Test the configure_data method of the NNBandsOnly model."""
-        model = NNBandsOnly(seed=42)
+        model = NNBandsOnly(seed=42, var='test_var')
         X_configured, y_configured = model.configure_data(X, y)
 
         # Check if the data is configured correctly
@@ -48,7 +65,5 @@ class TestNNBandsOnly:
         assert isinstance(y_configured, torch.Tensor)
 
         assert X_configured.shape == (3, 9)
-        assert y_configured.shape == torch.Size([3, 1])
+        assert y_configured.shape == (3, 1)
 
-        # Check if the data is scaled
-        assert torch.all(X_configured >= 0) and torch.all(X_configured <= 1)
